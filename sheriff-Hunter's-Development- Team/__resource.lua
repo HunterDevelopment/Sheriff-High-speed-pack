0f5e33113fc438042be4098c2e411f72f25fd9ea
Hunter's Development <104064537+HunterDevelopment@users.noreply.github.com>
2022-07-12 04:29:17 -0500

    Add files via upload
    Made By Hunter From Hunter's Development Have Fun!!    https://discord.gg/bRWKRcG8s2

---
+++ sheriff-Hunter's-Development- Team/__resource.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+game 'gta5'
+ author 'Hunter'
+ 
+description 'Made By Hunter From Hunters Development Team Have Fun!!      https://discord.gg/bRWKRcG8s2'
+
+files {
+    'data/**/*.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'DLC_TEXT_FILE' 'data/**/dlctext.meta'
+
+client_script "@Badger-Anticheat-master/acloader.lua"
